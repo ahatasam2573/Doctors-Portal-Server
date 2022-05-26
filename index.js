7449913079ffa765b35b26a6a4c6856f079d78d0
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-27 00:50:09 +0600

    updated and secure token

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qf0in.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
 async function run() {
     try {
         await client.connect();
@@ -65,6 +67,8 @@ async function run() {
 
         app.get('/booking', async (req, res) => {
             const patient = req.query.patient;
+            const authorization = req.headers.authorization;
+            console.log(authorization)
             const query = { patient: patient };
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
